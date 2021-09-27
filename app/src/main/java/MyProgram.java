cc00e72550aaa912e91b44688129ab8b94c500cc
arelive <me@are.moe>
2021-09-27 05:49:32 +0300

    complete the 1st task of practicum

--- app/src/main/java/MyProgram.java
+++ app/src/main/java/MyProgram.java
@@ -1,6 +1,17 @@
-public class MyProgram{
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
-	}
+import java.util.Scanner;
+
+public class MyProgram
+{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter two numbers");
+
+        int x = in.nextInt();
+        int y = in.nextInt();
+
+        int sum = x + y;
+
+        System.out.println("Their sum is " + sum);
+    }
 }
- 
